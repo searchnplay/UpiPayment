237239d83d0296a5e120ec9d9fe12de3d2ac148c
2LetRoom <bhupendramourya66@gmail.com>
2023-09-03 14:47:07 +0530

    upgrade

--- app/src/main/java/com/snpinfo/upipaymentdemo/MainActivity.java
+++ app/src/main/java/com/snpinfo/upipaymentdemo/MainActivity.java
@@ -4,8 +4,6 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-import com.snpinfo.upipayment.UpiPayment;
-
 public class MainActivity extends AppCompatActivity {
 
     @Override
