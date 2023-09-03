f5c49ae1213ebc5767b8339c0cc663eab3db8062
2LetRoom <bhupendramourya66@gmail.com>
2023-09-03 10:46:09 +0530

    upgrade

--- app/src/main/java/com/snpinfo/upipaymentdemo/MainActivity.java
+++ app/src/main/java/com/snpinfo/upipaymentdemo/MainActivity.java
@@ -4,13 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.snpinfo.upipayment.UpiPayment;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
     }
 }
